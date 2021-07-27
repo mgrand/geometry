333ac456a237d0b5ca9011bb830efd4156d51071
mgrand-softvision <mark.grand@softvision.com>
2021-07-27 15:19:56 -0400

    cross

--- src/main/scala/com/markgrand/geometry/Point.scala
+++ src/main/scala/com/markgrand/geometry/Point.scala
@@ -12,4 +12,15 @@ case class Point (x: Double, y: Double) {
    * @return the difference as a magnitude
    */
   def - (that: Point): Magnitude2d = Magnitude2d(this.x - that.x, this.y - that.y)
+
+  /**
+   * Define the 2-dimensional vector cross product v × w to be v<sub>x</sub>w<sub>y</sub> − v<sub>y</sub>w<sub>x</sub>
+   * as described in
+   * <a href="https://stackoverflow.com/questions/563198/how-do-you-detect-where-two-line-segments-intersect">stack
+   * overflow</a>
+   *
+   * @param that the other point
+   * @return the cross product
+   */
+  def cross(that: Point): Double = this.x*that.y - this.y*that.x
 }
--- src/test/scala/com/markgrand/geometry/PointTest.scala
+++ src/test/scala/com/markgrand/geometry/PointTest.scala
@@ -3,11 +3,23 @@ package com.markgrand.geometry
 import org.scalatest.FunSuite
 
 class PointTest extends FunSuite {
+  private val p1 = Point(3,4)
+  private val p2 = Point(7,11)
 
   test("test$minus") {
-    val p1 = Point(3,4)
-    val p2 = Point(7,11)
     assert((p2-p1) == Magnitude2d(4,7))
+  }
+
+  test( "cross") {
+    assertResult(0.0d) {
+      p1 cross p1
+    }
+    assertResult(3.0d*11 - 7*4) {
+      p1 cross p2
+    }
+    assertResult(7.0d*4 - 3*11) {
+      p2 cross p1
+    }
   }
 
 }
