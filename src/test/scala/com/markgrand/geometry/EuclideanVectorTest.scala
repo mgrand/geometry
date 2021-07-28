46016f5a56387fab419f8fd63999c4e03190f032
mgrand-softvision <mark.grand@softvision.com>
2021-07-28 11:48:56 -0400

    slope test

--- src/test/scala/com/markgrand/geometry/EuclideanVectorTest.scala
+++ src/test/scala/com/markgrand/geometry/EuclideanVectorTest.scala
@@ -1,9 +1,13 @@
 package com.markgrand.geometry
 
-import org.scalatest.FunSuite
 import org.scalatest.freespec.AnyFreeSpec
 
 class EuclideanVectorTest extends AnyFreeSpec {
+  private val v1 = EuclideanVector(Point(1, 1), Point(3, 3))
+  private val v2 = EuclideanVector(Point(-1, -1), Point(-3, -3))
+  private val v3 = EuclideanVector(Point(1, -1), Point(-1, 1))
+  private val v4 = EuclideanVector(Point(-1, 1), Point(1, -1))
+
   "isDegenerate" - {
     val A = Point(3, 4)
     val B = Point(6, 8)
@@ -18,8 +22,6 @@ class EuclideanVectorTest extends AnyFreeSpec {
   }
 
   "intersection" - {
-    val v1 = EuclideanVector(Point(1, 1), Point(3, 3))
-    val v2 = EuclideanVector(Point(-1, -1), Point(-3, -3))
     "Given two collinear vectors" - {
       "if they don't overlap, then there is no intersection" in {
         assertResult(NoIntersection()) {
@@ -53,21 +55,55 @@ class EuclideanVectorTest extends AnyFreeSpec {
           v2.intersection(v1)
         }
         assertResult(NoIntersection()) {
-          v1.intersection(EuclideanVector(Point(7,13), Point(23,11)))
+          v1.intersection(EuclideanVector(Point(7, 13), Point(23, 11)))
         }
         assertResult(NoIntersection()) {
-          v2.intersection(EuclideanVector(Point(7,13), Point(23,11)))
+          v2.intersection(EuclideanVector(Point(7, 13), Point(23, 11)))
         }
       }
       "If they do intersect, the intersection point should be returned" in {
-        assertResult(IntersectionPoint(Point(2.0,2.0))) {
-          v1.intersection(EuclideanVector(Point(1,3), Point(3,1)))
+        assertResult(IntersectionPoint(Point(2.0, 2.0))) {
+          v1.intersection(EuclideanVector(Point(1, 3), Point(3, 1)))
         }
-        assertResult(IntersectionPoint(Point(-2.0,-2.0))) {
-          v2.intersection(EuclideanVector(Point(-1,-3), Point(-3,-1)))
+        assertResult(IntersectionPoint(Point(-2.0, -2.0))) {
+          v2.intersection(EuclideanVector(Point(-1, -3), Point(-3, -1)))
         }
       }
     }
   }
 
+  "slope" - {
+    "Vectors with a non-zero x delta should return the expected numeric value" in {
+      assertResult(1.0) {
+        v1.slope
+      }
+      assertResult(1.0) {
+        v2.slope
+      }
+      assertResult(-1.0) {
+        v3.slope
+      }
+      assertResult(-1.0) {
+        v4.slope
+      }
+    }
+    "for a vector with a zero x delta and a positive y delta the result should be positive infinity" in {
+      val yp = EuclideanVector(Point(1, 1), Point(1, 8))
+      assertResult(Double.PositiveInfinity) {
+        yp.slope
+      }
+    }
+    "for a vector with a zero x delta and a negative y delta the result should be negative infinity" in {
+      val yp = EuclideanVector(Point(1, 8), Point(1, 1))
+      assertResult(Double.NegativeInfinity) {
+        yp.slope
+      }
+    }
+    "for a vector with a zero x delta and a zero y delta the result should be zero" in {
+      val yp = EuclideanVector(Point(1, 1), Point(1, 1))
+      assertResult(0.0) {
+        yp.slope
+      }
+    }
+  }
 }
