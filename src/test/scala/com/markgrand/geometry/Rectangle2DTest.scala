b63cac9e2ecd76ca6774757987c16d6524ab44a0
mgrand-softvision <mark.grand@softvision.com>
2021-07-28 15:29:05 -0400

    rectangle validation

--- src/test/scala/com/markgrand/geometry/Rectangle2DTest.scala
+++ src/test/scala/com/markgrand/geometry/Rectangle2DTest.scala
@@ -9,5 +9,13 @@ class Rectangle2DTest extends AnyFreeSpec {
       assert(null != Rectangle2D(Point(-2, 2), Point(2, 2), Point(2, -2), Point(-2,-2)))
       assert(null != Rectangle2D(Point(3, 0), Point(6, 3), Point(3, 6), Point(0,3)))
     }
+    "with unequal alternate side lengths" in {
+      assertThrows[IllegalArgumentException](Rectangle2D(Point(-2, 2), Point(2, 2), Point(2, -3), Point(-2,-2)))
+      assertThrows[IllegalArgumentException](Rectangle2D(Point(-2, 2), Point(2, 2), Point(2, -2), Point(-2,-3)))
+      assertThrows[IllegalArgumentException](Rectangle2D(Point(3, 0), Point(6, 3), Point(3, 7), Point(0,4)))
+    }
+    "parallelogram" in {
+      assertThrows[IllegalArgumentException](null != Rectangle2D(Point(-2, 2), Point(2, 2), Point(3, -2), Point(-1,-2)))
+    }
   }
 }
