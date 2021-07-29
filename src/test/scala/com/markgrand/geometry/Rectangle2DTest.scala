3d9c5a9747a2aefb2b6c7b032a0fbd456e36c786
mgrand-softvision <mark.grand@softvision.com>
2021-07-28 20:40:54 -0400

    contains rectangle

--- src/test/scala/com/markgrand/geometry/Rectangle2DTest.scala
+++ src/test/scala/com/markgrand/geometry/Rectangle2DTest.scala
@@ -3,6 +3,10 @@ package com.markgrand.geometry
 import org.scalatest.freespec.AnyFreeSpec
 
 class Rectangle2DTest extends AnyFreeSpec {
+  private val v1 = Rectangle2D(Point(-2, 2), Point(2, 2), Point(2, -2), Point(-2,-2))
+  private val v2 = Rectangle2D(Point(3, 0), Point(6, 3), Point(3, 6), Point(0,3))
+  private val v20 = Rectangle2D(Point(0, 20), Point(10, 20), Point(10, 10), Point(0, 10))
+
   "Create a rectangle" - {
     "with valid corner points" in {
       assert(null != Rectangle2D(Point(-2, 2), Point(2, 2), Point(2, -2), Point(-2,-2)))
@@ -20,15 +24,11 @@ class Rectangle2DTest extends AnyFreeSpec {
 
   "intersectionPoints" - {
     "Rectangles that do not intersect should produce an empty set of intersection points" in {
-      val v1 = Rectangle2D(Point(-2, 2), Point(2, 2), Point(2, -2), Point(-2,-2))
-      val v2 = Rectangle2D(Point(0, 20), Point(10, 20), Point(10, 10), Point(0, 10))
       assertResult(Set.empty[Point]) {
-        v1 intersectionPoints v2
+        v1 intersectionPoints v20
       }
     }
     "Rectangles that do intersect should produce a correct set of intersection points" in {
-      val v1 = Rectangle2D(Point(-2, 2), Point(2, 2), Point(2, -2), Point(-2,-2))
-      val v2 = Rectangle2D(Point(3, 0), Point(6, 3), Point(3, 6), Point(0,3))
       assertResult(Set(Point(1,2), Point(2, 1))) {
         v1 intersectionPoints v2
       }
@@ -36,8 +36,6 @@ class Rectangle2DTest extends AnyFreeSpec {
   }
 
   "contains Point" - {
-    val v1 = Rectangle2D(Point(-2, 2), Point(2, 2), Point(2, -2), Point(-2,-2))
-    val v2 = Rectangle2D(Point(3, 0), Point(6, 3), Point(3, 6), Point(0,3))
     "A point that is contained" in {
       assert(v1.contains(Point(0,0)))
       assert(v2.contains(Point(3,1)))
@@ -53,6 +51,21 @@ class Rectangle2DTest extends AnyFreeSpec {
     "An edge point is not contained" in {
       assert(!v1.contains(Point(0, 2)))
       assert(!v2.contains(Point(4, 1)))
+    }
+  }
+
+  "Contains Rectangle" - {
+    "A rectangle that is contained" in {
+      assert(v1.contains(Rectangle2D(Point(-1, 1), Point(1, 1), Point(1, -1), Point(-1, -1))))
+      assert(v2.contains(Rectangle2D(Point(2.5, 3.5), Point(3.5, 3.5), Point(3.5, 2.5), Point(2.5, 2.5))))
+    }
+    "A rectangle outside of is not contained" in {
+      assert(!v1.contains(Rectangle2D(Point(10, 10), Point(11, 10), Point(11, 9), Point(10, 9))))
+      assert(!v2.contains(Rectangle2D(Point(10, 10), Point(11, 10), Point(11, 9), Point(10, 9))))
+    }
+    "An intersecting rectangle is not contained" in {
+      assert(!v1.contains(Rectangle2D(Point(-5, 1), Point(5, 1), Point(5, 0), Point(-5, 0))))
+      assert(!v2.contains(Rectangle2D(Point(-5, 1), Point(5, 1), Point(5, 0), Point(-5, 0))))
     }
   }
 }
