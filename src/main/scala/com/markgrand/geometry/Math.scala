f946f93115a3a8316abd30e53759b998ec226b16
mgrand-softvision <mark.grand@softvision.com>
2021-07-31 10:48:55 -0400

    Math comments

--- src/main/scala/com/markgrand/geometry/Math.scala
+++ src/main/scala/com/markgrand/geometry/Math.scala
@@ -2,8 +2,13 @@ package com.markgrand.geometry
 
 /**
  * Math functions used elsewhere in this package.
+ *
+ * @author Mark Grand
  */
 private[geometry] object Math {
+  /**
+   * Equality testing functions in this class will treat values as equal if they differ by less than this amount.
+   */
   val Delta = 1e-10d
 
   def isZero(d: Double): Boolean = d.abs < Delta
